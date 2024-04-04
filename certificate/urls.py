d0e93b016eb3b2f84d5bc48dc61f7d00b9e552e7
mdrahuman06 <ragu20140610@gmail.com>
2024-04-04 14:06:20 +0530

    updates

---
+++ certificate/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.login_view,name="login"),
+    path('home/',views.home_view,name="home"),
+    path('success/',views.success_view,name="success"),
+    path('logout/',views.logout_view,name="logout"),
+    path('search/',views.search_view,name="search"),
+    path('certificate/<int:id>',views.certificate_view,name="certificate"),
+    path('user_data/<int:user_id>/',views.user_data_view, name='user_data'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
